27a09fe5eab274522d616d7dbb2c75c24099d186
Dennis Muturi <denismutur@gmail.com>
2024-01-24 15:21:16 +0300

    Add scroll functionality and clear input field after sending message

--- src/components/UserMessages.jsx
+++ src/components/UserMessages.jsx
@@ -1,12 +1,19 @@
 import { useLocation, useOutletContext, useParams } from "react-router-dom"
 import rightArrow from '../assets/sendArrow.svg';
-import { useState } from "react";
+import { useRef, useState,useEffect } from "react";
 import axios  from "axios";
 const UserMessages = () => {
     const { id } = useParams();
     const context=useOutletContext();
     const [message,setMessage]=useState();
     const location=useLocation();
+    const inputElement=useRef();
+    const messagesEndRef=useRef();
+    const scrollToBottom = () => {
+      messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    };
+
+    useEffect(scrollToBottom, [context]);
     const handleSend=async ()=>{
       const response=await axios.post('http://localhost:4000/sendmessage',
       {
@@ -15,6 +22,7 @@ const UserMessages = () => {
       },
       {withCredentials:true})
       context[1].addMessage({senderid:1,message:message,receiverid:id,time:new Date()});
+      inputElement.current.value=''
     }
   return (
     <div className="messages" >
@@ -29,8 +37,10 @@ const UserMessages = () => {
           return <div key={index} className="right"><span>{usermessage.message}</span><span>{usermessage.time&&new Date(usermessage.time).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</span></div>
         }
       }
-    })}</div>
-    <div className="sendMessage"><input type="text" placeholder="Enter message here"  onChange={(e)=>setMessage(e.target.value)}/><img onClick={handleSend} src={rightArrow} alt="" /></div>
+    })}
+        <div ref={messagesEndRef}></div>
+    </div>
+    <div className="sendMessage"><input type="text" ref={inputElement} placeholder="Enter message here"  onChange={(e)=>setMessage(e.target.value)}/><img onClick={handleSend} src={rightArrow} alt="" /></div>
     </div>
   )
 }
