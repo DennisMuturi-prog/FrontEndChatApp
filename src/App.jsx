125597c207ae7b4f935a36848dbbf67865f45662
Dennis Muturi <denismutur@gmail.com>
2024-01-24 12:58:18 +0300

    Add App component with routes and components

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import { Route,Routes } from 'react-router-dom'
+import Login from './components/Login'
+import Register from './components/Register'
+import UsersArea from './components/UsersArea'
+import UserMessages from './components/UserMessages'
+import DefaultMessage from './components/DefaultMessage'
+
+function App() {
+  return (
+    <>
+    <nav>
+      <h1>My chat App</h1>
+    </nav>
+    
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/users' element={<UsersArea/>}>
+        <Route index element={<DefaultMessage/>}/>
+        <Route path=':id' element={<UserMessages/>}/>
+      </Route>  
+    </Routes>
+    </>
+    
+
+  )
+}
+
+export default App
