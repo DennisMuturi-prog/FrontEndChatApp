06cd1bb7ece27ded112b51cbe8df4a5f7d422eef
Dennis Muturi <denismutur@gmail.com>
2024-01-28 22:49:08 +0300

    Refactor UserMessages component to display user messages and handle message sending

--- src/components/UserMessages.jsx
+++ src/components/UserMessages.jsx
@@ -1,19 +1,25 @@
 import { useLocation, useOutletContext, useParams } from "react-router-dom"
 import rightArrow from '../assets/sendArrow.svg';
 import { useRef, useState,useEffect } from "react";
+import userIcon from '../assets/user.svg';
 import axios  from "axios";
 const UserMessages = () => {
     const { id } = useParams();
     const context=useOutletContext();
     const [message,setMessage]=useState();
+    const [messages,setMessages]=useState(context[0].usermessages);
     const location=useLocation();
     const inputElement=useRef();
     const messagesEndRef=useRef();
     const scrollToBottom = () => {
       messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
     };
-
-    useEffect(scrollToBottom, [context]);
+    useEffect(()=>{
+      const messages=context[0].usermessages.filter(message=>message.receiverid==id || message.senderid==id);
+      setMessages(messages);
+    },[id,context])
+    
+    useEffect(scrollToBottom,[context]);
     const handleSend=async ()=>{
       const response=await axios.post('http://localhost:4000/sendmessage',
       {
@@ -26,20 +32,22 @@ const UserMessages = () => {
     }
   return (
     <div className="messages" >
-      <h1>{location.state.friendname}</h1>
+      <h1>
+        <img src={location.state.imageUrl?location.state.imageUrl:userIcon}/>
+        {location.state.friendname}</h1>
       <div className="messagesbody">
-        {context[0].usermessages[0]&&<div className="dateDiv">{context[0].usermessages[0].time && new Date(context[0].usermessages[0].time).toLocaleDateString()}</div>}
-    {context[0].usermessages.map((usermessage,index)=>{
-      const messageDate = new Date(usermessage.time);
-      const today = new Date();
-      const displayDate = 
-        messageDate.getDate() === today.getDate() &&
-        messageDate.getMonth() === today.getMonth() &&
-        messageDate.getFullYear() === today.getFullYear()
-          ? "Today"
-          : messageDate.toLocaleDateString();
-      return(
-        (index>0 && new Date(usermessage.time).getDate()>new Date(context[0].usermessages[index-1].time).getDate())
+        {messages[0] &&<div className="dateDiv">{new Date(context[0].usermessages[0].time).toLocaleDateString()}</div>}
+        {messages.map((usermessage,index)=>{
+          const messageDate = new Date(usermessage.time);
+          const today = new Date();
+          const displayDate = 
+            messageDate.getDate() === today.getDate() &&
+            messageDate.getMonth() === today.getMonth() &&
+            messageDate.getFullYear() === today.getFullYear()
+           ? "Today"
+           : messageDate.toLocaleDateString();
+        return(
+        (index>0 && new Date(usermessage.time).getDate()>new Date(messages[index-1].time).getDate())
         ?<><div className="dateDiv">{displayDate}</div>
         {
           (usermessage.senderid==id || usermessage.receiverid==id)
@@ -61,7 +69,8 @@ const UserMessages = () => {
     })}  
         <div ref={messagesEndRef}></div>
     </div>
-    <div className="sendMessage"><input type="text" ref={inputElement} placeholder="Enter message here"  onChange={(e)=>setMessage(e.target.value)}/><img onClick={handleSend} src={rightArrow} alt="" /></div>
+    <div className="sendMessage"><input type="text" ref={inputElement} placeholder="Enter message here"  onChange={(e)=>setMessage(e.target.value)}/><img onClick={handleSend} src={rightArrow} alt="" />
+    </div>
     </div>
   )
 }
