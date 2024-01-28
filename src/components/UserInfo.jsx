41c9bb1b09a3a71f04813d9e9b7e3f741278e642
Dennis Muturi <denismutur@gmail.com>
2024-01-28 22:48:46 +0300

    Add latest message display in UserInfo component

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,16 +1,23 @@
 import {  useLocation, useNavigate, useParams } from "react-router-dom"
 import userIcon from '../assets/user.svg';
-const UserInfo = ({userdata}) => {
+import { useEffect,useState } from "react";
+const UserInfo = ({userdata,usermessages}) => {
     const {id}=useParams();
     const location=useLocation();
     const navigate=useNavigate();
+    const [latestMessage, setLatestMessage] = useState(null);
+    useEffect(() => {
+        // Get the latest message that matches the senderId or receiverId with userdata._id
+        const message=usermessages.filter(message=>message.senderid==userdata._id || message.receiverid==userdata._id);
+        setLatestMessage(message[message.length-1]);
+    }, [usermessages]);
     const handleClick=()=>{
-        navigate(`/users/${userdata._id}`,{state:{username:location.state.username,friendname:userdata.username}});
+        navigate(`/users/${userdata._id}`,{state:{username:location.state.username,friendname:userdata.username,myimageUrl:location.state.myimageUrl,imageUrl:userdata.imageUrl}});
     }
   return (
     <div className={userdata._id==id?"usercardActive":"usercard"} onClick={handleClick}>
       <img src={userdata.imageUrl?userdata.imageUrl:userIcon}/>
-      {userdata.username}
+      <div className="latestmessage"><span>{userdata.username}</span><span>{latestMessage ? latestMessage.message : 'No messages yet'}</span></div> 
     </div>
   )
 }
