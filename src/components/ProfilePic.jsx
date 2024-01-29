1fe4a83cbb579b1578073f17299aabc6610b2ae3
Dennis Muturi <denismutur@gmail.com>
2024-01-30 01:31:14 +0300

    Add styles to ProfilePic component

--- src/components/ProfilePic.jsx
+++ src/components/ProfilePic.jsx
@@ -1,6 +1,7 @@
 import ReactCrop, { centerCrop, convertToPixelCrop, makeAspectCrop } from 'react-image-crop'
 import 'react-image-crop/dist/ReactCrop.css'
 import setCanvasPreview from './setCanvasPreview';
+import styles from './ProfilePic.module.css'
 import { useRef, useState } from "react";
 import axios from 'axios';
 import { useLocation, useNavigate } from 'react-router-dom';
@@ -42,13 +43,17 @@ const ProfilePic = () => {
         setCrop(centeredCrop);
     }
   return (
-    <>
-    <div>
+    <div className={styles.mainPic}>
+    <div className={styles.choosePhotoDiv}>
         <label>
             <span>Choose Profile Pic</span>
             <input type="file"  accept="image/*"
             onChange={onSelectFile} />
+            <span onClick={()=>{
+                navigate('/users',{state:{username:location.state.username}});
+            }}className={styles.skipBtn}>Skip</span>
         </label>
+        
     </div>
     {imgSrc&&
     <div>
@@ -63,31 +68,33 @@ const ProfilePic = () => {
         minWidth={MIN_DIMENSION}>
             <img src={imgSrc} ref={imgRef} alt="upload" style={{maxHeight:"70vh"}} onLoad={onImageLoad}/>
         </ReactCrop>
-        <button
-        onClick={()=>{
-            setCanvasPreview(
-                imgRef.current,
-                previousCanvasRef.current,
-                convertToPixelCrop(
-                    crop,
-                    imgRef.current.width,
-                    imgRef.current.height
-                )
-            );
-            setShowUploadBtn(true);
-        }}>crop Image</button>
-        {showUploadBtn&&<button
-        onClick={async ()=>{
-            const dataUrl=previousCanvasRef.current.toDataURL();
-            const response=await axios.post('http://localhost:4000/profilepic',
-            {imageUrl:dataUrl},
-            {withCredentials:true}
-            ) ;
-            if(response.data.message=='successfully added profile pic'){
-                navigate('/users',{state:{username:location.state.username,myimageUrl:response.data.imageUrl}});
+        <div className={styles.cropBtns}>
+            <button
+            onClick={()=>{
+                setCanvasPreview(
+                    imgRef.current,
+                    previousCanvasRef.current,
+                    convertToPixelCrop(
+                        crop,
+                        imgRef.current.width,
+                        imgRef.current.height
+                    )
+                );
+                setShowUploadBtn(true);
+            }}>crop Image</button>
+            {showUploadBtn&&<button
+            onClick={async ()=>{
+                const dataUrl=previousCanvasRef.current.toDataURL();
+                const response=await axios.post('http://localhost:4000/profilepic',
+                {imageUrl:dataUrl},
+                {withCredentials:true}
+                ) ;
+                if(response.data.message=='successfully added profile pic'){
+                    navigate('/users',{state:{username:location.state.username,myimageUrl:response.data.imageUrl}});
+                }
             }
-        }
-        }>Upload profile pic</button>}
+            }>Upload profile pic</button>}
+        </div>
     </div>
     }
     {crop&&
@@ -100,7 +107,7 @@ const ProfilePic = () => {
             height:150
         }}/>
     }
-    </>
+    </div>
   )
 }
 
