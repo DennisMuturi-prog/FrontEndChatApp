9bf4cb703cb004e7f5e007c71705a3dac29cfcab
Dennis Muturi <denismutur@gmail.com>
2024-02-01 12:51:37 +0300

    Update socket URL in socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client'
 import Cookies from 'js-cookie';
-export const socket = io('http://backendchatapp-production-b4cd.up.railway.app', {
+export const socket = io('https://backendchatapp-ghen.onrender.com', {
   auth: { token: Cookies.get('token') },
   autoConnect: false,
 });
