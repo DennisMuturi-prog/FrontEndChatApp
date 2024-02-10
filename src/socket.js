8dfe05c91558f379987d59701e947d0a5061ecc0
Dennis Muturi <denismutur@gmail.com>
2024-02-10 14:45:37 +0300

    Update socket URL in socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import {io} from 'socket.io-client'
-export const socket = io('https://backendchatapp-ghen.onrender.com', {
+export const socket = io('http://localhost:4000', {
   withCredentials: true,
   autoConnect: false,
 });
