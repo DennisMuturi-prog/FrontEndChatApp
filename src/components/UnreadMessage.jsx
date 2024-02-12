b4cb9dd82ea21c423ad616fa6aa1cbd48e55cb00
Dennis Muturi <denismutur@gmail.com>
2024-02-12 14:02:03 +0300

    Add UnreadMessage component

---
+++ src/components/UnreadMessage.jsx
@@ -0,0 +1,28 @@
+import  { useRef,useEffect } from "react";
+import axios from "axios";
+export function UnreadMessage({
+  usermessage,
+  index
+}) {
+    const unreadMessage=useRef();
+    useEffect(()=>{
+      const observer=new IntersectionObserver(entries=>{
+        const entry=entries[0];
+        if(entry.isIntersecting){
+          readMessage(usermessage._id);
+        }
+      });
+    observer.observe(unreadMessage.current);
+    },[])
+    const readMessage=async (messageid)=>{
+      await axios.post('http://localhost:4000/readMessage',
+          {_id:messageid},
+          {withCredentials:true}  );
+
+    }
+  return <div ref={unreadMessage} key={index} className="left"><span>{usermessage.message}</span><span>{usermessage.time && new Date(usermessage.time).toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit'
+      })}</span></div>;
+}
+  
